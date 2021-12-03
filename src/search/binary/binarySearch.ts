a80415e58b6b1a15240a2cb03b9326b8d98ed0bc
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-12-03 09:59:43 +0000

    doing binary search

--- src/search/binary/binarySearch.ts
+++ src/search/binary/binarySearch.ts
@@ -1,25 +1,29 @@
-import { NO_MATCH } from "../common"
+import { NO_MATCH } from "../common";
 
 const binarySearch = (items: any[], item: any): number => {
-    let index: number = -1;
-    let found: boolean = false;
+    let result: number = -1;
     let first: number = 0;
     let last: number = items.length - 1;
 
-
-    while(found=false && first<last) {
-        let midpoint: number = (first+last)/2
-        if(items[midpoint] === items){
-            index==midpoint;
-            found==true;
-        }else if(items[midpoint]<item) {
-            first = midpoint + 1
-        }else if(items[midpoint]>item) {
-            last = midpoint - 1
-        }
+    while(result == -1){
+       let midpoint: number = (last + first)/2;
+       if(item == items[midpoint]){
+           result == midpoint;
+       }else if(first == last){
+           result == NO_MATCH
+       }else if(item < items[midpoint]){
+           if(midpoint == 0){
+               result ==  NO_MATCH
+           }
+           last == midpoint -1
+       }else if(item > items[midpoint]){
+           if(midpoint == 0){
+               result == NO_MATCH
+           }
+           first == midpoint + 1
+       }
     }
-    return index;
-    return NO_MATCH;
+    return result;
 }
 
 
