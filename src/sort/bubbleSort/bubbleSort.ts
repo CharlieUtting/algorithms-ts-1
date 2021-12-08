763421566827b1c2cca469ffb336f281c9f85abb
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-12-08 10:50:06 +0000

    finished bubble sort

--- src/sort/bubbleSort/bubbleSort.ts
+++ src/sort/bubbleSort/bubbleSort.ts
@@ -4,7 +4,19 @@ import swap from "../swap";
 const bubbleSort: SortAlgorithm<any> = <T>(items: T[]): T[] => {
 
     // Your code here
-
+    for(let top = items.length; top>1;top = top-1){
+        let swapmade: boolean = false;
+        for(let index = 0; index < (top);index++){
+            if(items[index] > items[index+1]){
+                swapmade=true;
+                swap(items, index, index+1);
+            }
+        }
+        if(!swapmade){
+            break
+        }
+    }
+    
     return items;
 }
 
