9a0c2cf1e5dcd45e98d3125f459c9099ee4e5072
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-12-09 09:50:35 +0000

    started merge sort

--- src/sort/mergeSort/mergeSort.ts
+++ src/sort/mergeSort/mergeSort.ts
@@ -3,6 +3,34 @@ import { SortAlgorithm } from "../common";
 const mergeSort: SortAlgorithm<any> = <T>(items: T[]): T[] => {
 
     // Your code here
+    if(items.length == 0){
+        return items
+      }
+      return mergeSortRecurse<T>(items, 0, items.length - 1);
+  }
+  
+  const mergeSortRecurse = <T>(inputArr: T[], leftPointer: number, rightPointer: number) => {
+    if(leftpointer == rightpointer){
+      return [inputArr[leftpointer]]
+    }
+    let midpoint: number = math.floor((rightPointer+leftPointer)/2)
+    const firstHalf: T[] = mergeSortRecurse(inputArr, leftPointer, midpoint)
+    const secondHalf: T[] = mergeSortRecurse(inputArr, midpoint + 1, rightPointer)
+  
+    const outputList = []
+  
+    while(firstHalf.length > 0 && secondHalf.length > 0){
+      if(firstHalf[0] > secondHalf[0]){
+        outputList.push(secondHalf.shift())
+      } else {
+        outputList.push(secondHalf.shift)
+      }
+    }
+    outputList.push(firstHalf)
+    outputList.push(secondHalf)
+  
+    return outputList
+  }
 
     return items;
 }
